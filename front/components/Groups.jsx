ec34c5bfb990bb87812ecb15ec1505d0f11440b7
baxy5 <bakos.soda@gmail.com>
2022-09-06 11:47:38 +0200

    strapi home tiles api

--- front/components/Groups.jsx
+++ front/components/Groups.jsx
@@ -1,12 +1,44 @@
 import React from "react";
+import Link from "next/link";
 
 const Groups = () => {
   return (
-    <div>
-      <div>asd</div>
-      <div>asd</div>
-      <div>asd</div>
-      <div>asd</div>
+    <div className="bg-transparent p-2 md:flex md:gap-3">
+      <div className="card text-[#9F844E] h-full text-center p-2 py-10 my-5">
+        <h2 className="text-xl mb-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+        </h2>
+        <Link href="/about">
+          <a className="text-md uppercase">Rólam</a>
+        </Link>
+      </div>
+
+      <div className="card text-[#9F844E] h-full text-center p-2 py-10 my-5">
+        <h2 className="text-xl mb-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+        </h2>
+        <Link href="/about">
+          <a className="text-md uppercase">Rólam</a>
+        </Link>
+      </div>
+
+      <div className="card text-[#9F844E] h-full text-center p-2 py-10 my-5">
+        <h2 className="text-xl mb-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+        </h2>
+        <Link href="/about">
+          <a className="text-md uppercase">Rólam</a>
+        </Link>
+      </div>
+
+      <div className="card text-[#9F844E] h-full text-center p-2 py-10 my-5">
+        <h2 className="text-xl mb-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.{" "}
+        </h2>
+        <Link href="/about">
+          <a className="text-md uppercase">Rólam</a>
+        </Link>
+      </div>
     </div>
   );
 };
--- front/components/Hero.jsx
+++ front/components/Hero.jsx
@@ -2,17 +2,27 @@ import React from "react";
 import Link from "next/link";
 import Image from "next/image";
 
+import Groups from "../components/Groups";
+
 const Hero = () => {
   return (
-    <div className="main-nav h-full flex justify-center items-center text-center py-24">
-      <div>
+    <div className="main-nav h-full">
+      <div className="flex justify-center items-center text-center py-24">
         <div>
-          <Image src="/logo.png" width="500" height="500" />
+          <div>
+            <Image
+              src="/logo.png"
+              width="500"
+              height="500"
+              alt="Énirányítok logo"
+            />
+          </div>
+          <Link href="#contact">
+            <a className="btn-one text-xl uppercase px-6 py-3">Elérhetőség</a>
+          </Link>
         </div>
-        <Link href="#contact">
-          <a className="btn-one text-xl uppercase px-6 py-3">Elérhetőség</a>
-        </Link>
       </div>
+      <Groups />
     </div>
   );
 };

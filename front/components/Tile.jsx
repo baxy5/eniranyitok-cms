cc3279a0793cda96f0726f0384be8df7c5c2cc0b
baxy5 <bakos.soda@gmail.com>
2022-09-25 16:48:45 +0200

    content type setting

--- front/components/Tile.jsx
+++ front/components/Tile.jsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 
 export function Tile({ title, text, href }) {
   return (
-    <div className="card flex items-center justify-center text-[#9F844E] h-56 text-center p-2 py-10 my-5 w-full">
+    <div className="card flex items-center justify-center text-[#9F844E] h-80 xl:h-56 text-center p-2 py-10 my-5 w-full">
       <div>
-        <h2 className="text-xl mb-5">{text} </h2>
+        <h2 className="lg:text-xl mb-5">{text} </h2>
         <Link href={href}>
           <a className="button font-bold text-md uppercase">{title}</a>
         </Link>

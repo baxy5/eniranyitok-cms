4440619db0ab77bc0c1a2d62a5d53d2ae365b05e
baxy5 <bakos.soda@gmail.com>
2023-03-19 19:08:21 +0100

    hipnozis

--- front/pages/hipnozis.js
+++ front/pages/hipnozis.js
@@ -1,32 +1,47 @@
-import React from 'react'
+import React from "react";
+import Layout from "../components/Layout";
+import HomeTiles from "../components/HomeTiles";
+import ReactMarkdown from "react-markdown";
 
-const Hipnozis = ({hipnosisData, aboutData, mainTilesData}) => {
+const Hipnozis = ({ hipnosisData, mainTilesData }) => {
   return (
-    <div>{hipnosisData.title}</div>
-  )
-}
+    <div>
+      <Layout>
+        <div className="h-full bg-[#161718] text-[#9f844e] p-3 pt-16 sm:p-5 sm:pt-20 md:px-28 lg:px-80">
+          <div>
+            <h1 className="text-3xl custom-underline mb-5 sm:text-5xl">
+              {hipnosisData.title}
+            </h1>
+          </div>
+          <div>
+            <ReactMarkdown>{hipnosisData.content}</ReactMarkdown>
+          </div>
+        </div>
+        <div className="bg-[#161718]">
+          <HomeTiles data={mainTilesData} />
+        </div>
+      </Layout>
+    </div>
+  );
+};
 
 export async function getServerSideProps() {
-    const [about, mainTiles, hipnosis] = await Promise.all([
-      fetch(process.env.NEXT_PUBLIC_ABOUT_API),
-      fetch(process.env.NEXT_PUBLIC_HOME_API),
-      fetch(process.env.NEXT_PUBLIC_HIPNOSIS_API)
-    ]);
-  
-    const aboutDataJSON = await about.json();
-    const mainTilesDataJSON = await mainTiles.json();
-    const hipnosisDataJSON = await hipnosis.json()
-    const aboutData = await aboutDataJSON.data.attributes.AboutSection[0];
-    const mainTilesData = await mainTilesDataJSON.data.attributes.HomeTiles[0];
-    const hipnosisData = await hipnosisDataJSON.data.attributes;
-  
-    return {
-      props: {
-        aboutData,
-        mainTilesData,
-        hipnosisData
-      },
-    };
+  const [mainTiles, hipnosis] = await Promise.all([
+    fetch(process.env.NEXT_PUBLIC_HOME_API),
+    fetch(process.env.NEXT_PUBLIC_HIPNOSIS_API),
+  ]);
+
+  const mainTilesDataJSON = await mainTiles.json();
+  const hipnosisDataJSON = await hipnosis.json();
+  const mainTilesData = await mainTilesDataJSON.data.attributes.HomeTiles[0];
+  const hipnosisData = await hipnosisDataJSON.data.attributes;
+
+  return {
+    props: {
+      mainTilesData,
+      hipnosisData,
+    },
+  };
 }
 
-export default Hipnozis
+export default Hipnozis;

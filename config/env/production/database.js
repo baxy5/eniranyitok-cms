fee83ab312dee07707fb8834765c5991be0beb91
baxy5 <bakos.soda@gmail.com>
2025-10-04 22:12:59 +0200

    maybe fix

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,21 +1,29 @@
-const parse = require('pg-connection-string').parse;
+const parse = require("pg-connection-string").parse;
 
-let config = parse(process.env.DATABASE_URL)
+module.exports = ({ env }) => {
+  let config = {};
 
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connection: {
-    client: 'postgres',
+  if (process.env.DATABASE_URL) {
+    config = parse(process.env.DATABASE_URL);
+  }
+
+  return {
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
+      client: "postgres",
+      connection: {
+        host: config.host || env("DATABASE_HOST", "127.0.0.1"),
+        port: config.port || env.int("DATABASE_PORT", 5432),
+        database: config.database || env("DATABASE_NAME", "postgres"),
+        user: config.user || env("DATABASE_USERNAME", "postgres"),
+        password: config.password || env("DATABASE_PASSWORD", ""),
+        ssl:
+          process.env.NODE_ENV === "production"
+            ? {
+                rejectUnauthorized: false,
+              }
+            : false,
       },
+      debug: false,
     },
-    debug: false,
-  },
-});
+  };
+};

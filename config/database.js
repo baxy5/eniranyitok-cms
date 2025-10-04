c6b5615620e88cefcf172bc7a7df286a808861f7
baxy5 <bakos.soda@gmail.com>
2025-10-04 21:51:43 +0200

    db

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => {
         port: env.int("DATABASE_PORT", 5432),
         database: env("DATABASE_NAME", "postgres"),
         user: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "asd123"),
+        password: env("DATABASE_PASSWORD", ""),
         ssl: false,
       },
     },
